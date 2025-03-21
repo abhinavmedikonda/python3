ec16a03c784c0722777222d73ecc8f81c3f59438
abhinav medikonda <abhinavmedikonda@hotmail.com>
2025-03-20 21:53:21 -0400

    * jsons

--- jsons.py
+++ jsons.py
@@ -10,7 +10,7 @@ people = requests.get('http://api.open-notify.org/astros.json')
 json = people.json()
 
 print('people currently in space:')
-for item in json['people']:
+for item in json.get('people'):
     print(item['name'], '-', item['craft'])
 
 
